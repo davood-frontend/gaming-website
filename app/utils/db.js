08ddab4896eb193de0ca4f93d0fba88e87927ec4
David <davood.frontend@gmail.com>
2024-05-02 10:33:18 -0700

    deploy

--- app/utils/db.js
+++ app/utils/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO)
+        await mongoose.connect(process.env.NEXT_PUBLIC_MONGO)
     } catch (err) {
         console.log("Database connection error");
     }
