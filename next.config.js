7e9720c2ccd2113072b851f9bde3020da635cfd9
David <davood.frontend@gmail.com>
2024-05-04 08:04:48 -0700

    debug

--- next.config.js
+++ next.config.js
@@ -4,9 +4,10 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "**", 
+                hostname: "**",
             },
         ],
     },
+    output: 'export'
 };
 module.exports = nextConfig;
