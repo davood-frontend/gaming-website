bdfcab8c6c4ab258af4102a6b8c4810bfa6e61d5
David <davood.frontend@gmail.com>
2024-05-07 05:19:54 -0700

    debug

--- app/(pages)/layout.js
+++ app/(pages)/layout.js
@@ -1,9 +1,5 @@
 import Footer from "@/components/homePage/Footer"
-import { envURl } from "../services/requests"
 export default function CostumLayout({ children }) {
-    if (!envURl) {
-        return null
-    }
     return (
 
         <>
--- app/(pages)/shop/page.js
+++ app/(pages)/shop/page.js
@@ -6,14 +6,11 @@ import HeroContainer from '@/components/shop/HeroContainer';
 import SuggestedProducts from '@/components/shop/SuggestedProducts';
 import Games from '@/components/shop/Games';
 import Consoles from '@/components/shop/Consoles';
-import { envURl } from '@/app/services/requests';
 export const metadata = {
     title: 'فروشگاه'
 }
 const page = () => {
-    if (!envURl) {
-        return null
-    }
+
     return (
         <Box sx={{ paddingX: { xs: '10px', sm: '20px', md: '10%' } }}>
             <BackGround img={bg} />
